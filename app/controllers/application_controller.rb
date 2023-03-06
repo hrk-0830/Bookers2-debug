390c85dafe908c5b71af5933097904d01d17c0ab
EC2 Default User <ec2-user@ip-172-31-46-23.ap-northeast-1.compute.internal>
2023-03-06 01:29:15 +0000

    [Task4]230306

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
-  end
-
-  def after_sign_up_path_for(resource)
-    user_path(current_user)
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
