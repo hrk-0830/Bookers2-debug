d4eac9c86bd7581025da080bb84c680a1a3334de
EC2 Default User <ec2-user@ip-172-31-46-23.ap-northeast-1.compute.internal>
2023-03-06 10:58:59 +0000

    [Task4]230306

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
 
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
-  
+
   def is_followed_by?(user)
-    reverse_of_relationships.find_by(followed_id: user.id).present?
+    reverse_of_relationships.find_by(following_id: user.id).present?
   end
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
